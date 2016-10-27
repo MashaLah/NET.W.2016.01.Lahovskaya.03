00b63eddddb682c2efbeb9a752d26f9823224333
MashaLah <lahoska91@mail.ru>
2016-10-28 00:08:20 +0300

    Add Task1.Tests

---
+++ Task1.Tests/NewtonSqrTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task1;
+
+namespace Task1.Tests
+{
+    [TestClass]
+    public class NewtonSqrTests
+    {
+        /// <summary>
+        /// A test for FindSqr() with valid parameters.
+        /// </summary>
+        [TestMethod]
+        public void FindSqr_Number16Degree4_Returned2()
+        {
+            //arrange
+            double number = 16;
+            double n = 4;
+            double e = 0;
+            double expected = 2;
+            //act
+            double actual = NewtonSqr.FindSqr(number, n, e);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// A test for FindSqr() with valid parameters<0.
+        /// </summary>
+        [TestMethod]
+        public void FindSqr_NumberMinus125Degree3_ReturnedMinus5()
+        {
+            //arrange
+            double number = -125;
+            double n = 3;
+            double e = 0;
+            double expected = -5;
+            //act
+            double actual = NewtonSqr.FindSqr(number, n, e);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Task1/NewtonSqr.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public static class NewtonSqr
+    {
+        /// <summary>
+        /// Finds root of a number with Newton's method.
+        /// </summary>
+        public static double FindSqr(double number, double n, double e)
+        {
+            double x0 = number / n;
+            double x = ((n - 1) * x0 + number / Math.Pow(x0, n - 1))/n;
+            while (Math.Abs(x - x0) > e)
+            {
+                x0 = x;
+                x = ((n - 1) * x0 + number / Math.Pow(x0, n - 1))/n;
+            }
+            return x;
+        }
+    }
+}
