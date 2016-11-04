2c54388d0b46bce81ac66b688f30f258f1a37cc2
MashaLah <lahoska91@mail.ru>
2016-11-04 22:50:50 +0300

    Add Task2.

---
+++ Task2/DoubleBinaryView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public static class DoubleBinaryView
+    {
+        /// <summary>
+        /// Extension method for Double, represents binary view.
+        /// </summary>
+        public static string ShowBinaryView(this double number)
+        {
+            byte[] byteArray = BitConverter.GetBytes(number);
+            BitArray bitArray = new BitArray(byteArray);
+            char[] resultArray = new char[sizeof(double)*8];
+            for (int i = 0; i <resultArray.Length; i++)
+            {
+                resultArray[i] = bitArray[i] ? '1' : '0';
+            }
+            Array.Reverse(resultArray);
+            return new String(resultArray);
+        }
+    }
+}
---
+++ Task2Tests/DoubleBinaryViewTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task2;
+
+namespace Task2Tests
+{
+    public class DoubleBinaryViewTests
+    {
+        /// <summary>
+        /// A test for ShowBinaryView().
+        /// </summary>
+        [TestCase(123.123, ExpectedResult = "0100000001011110110001111101111100111011011001000101101000011101")]
+        [TestCase(-123.123, ExpectedResult = "1100000001011110110001111101111100111011011001000101101000011101")]
+        [TestCase(0.0, ExpectedResult = "0000000000000000000000000000000000000000000000000000000000000000")]
+        [TestCase(-0.0, ExpectedResult = "1000000000000000000000000000000000000000000000000000000000000000")]
+        public string ShowBinaryView_ValidData_ValidResult(double number) => number.ShowBinaryView();
+    }
+}
